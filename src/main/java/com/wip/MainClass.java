3568436efef29773e35f2c4180ffb4e2c106ca88
Deva Harshini <Jan1user241@rpsconsulting.in>
2026-01-28 21:00:58 +0530

    jenkins file added

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Java for Docker! Code is pulled from GitHub Repository");
+		System.out.println("Updated code after adding Jenkins file to repository");
 
 	}
 
