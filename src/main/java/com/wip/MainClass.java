15e1aff35e3fe395d2e200eced172ec4a1987c1c
Deva Harshini <Jan1user241@rpsconsulting.in>
2026-01-28 20:11:02 +0530

    project is committed

---
+++ src/main/java/com/wip/MainClass.java
@@ -0,0 +1,11 @@
+package com.wip;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java for Docker! Code is pulled from GitHub Repository");
+
+	}
+
+}
